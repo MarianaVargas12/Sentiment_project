ac5cf71d7782fecce17a61c3a4f6cfb44203f787
Mariana <marianavargas1209@gmail.com>
2022-09-14 19:56:13 -0600

    [new] python function

---
+++ cloud_function/main.py
@@ -0,0 +1,47 @@
+def main(event, context):
+    from google.cloud import vision
+    import firebase_admin
+    from firebase_admin import firestore
+
+    file_name= event["name"]
+    bucket_name= event["bucket"]
+
+    client = vision.ImageAnnotatorClient() 
+
+    #URI of the image
+    blob_uri = f"gs://{bucket_name}/{file_name}"
+    #blob source
+    image = vision.Image(source=vision.ImageSource(image_uri=blob_uri))
+    response = client.face_detection(image=image)
+    faceAnnotation = response.face_annotations
+
+    #Get the first face
+    analist = faceAnnotation[0]
+
+    if (analist.detection_confidence >= 3) and (analist.detection_confidence <= 5):
+        answer = "confident"
+    elif (analist.anger_likelihood >= 3) and (analist.anger_likelihood <= 5):
+        answer = "angry"
+    elif (analist.joy_likelihood >= 3) and (analist.joy_likelihood <= 5):
+        answer = "happy"
+    elif (analist.sorrow_likelihood >= 3) and (analist.sorrow_likelihood <= 5):
+        answer = "sad"
+    elif (analist.surprise_likelihood >= 3) and (analist.surprise_likelihood <= 5):
+        answer = "surprised"
+
+    employee = file_name.split(".")[0]
+    print(employee + "is" + answer)
+
+    app_options = {'projectId': 'proyectosoa-362116'}
+    app = firebase_admin.initialize_app(options=app_options)
+    db = firestore.client()
+    doc = db.collection("employee").document(employee)
+
+    doc.set({
+        "name": employee,
+        "emotion": answer
+    })
+    
+    # Delete the default app 
+    firebase_admin.delete_app(app)
+
