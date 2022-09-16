bc0f36aafba27e9d8a2e3e06f790d1452a3145d1
Mario Araya <marioaxtrem@gmail.com>
2022-09-15 19:58:56 -0600

    se agrega la condicion de no se sabe la emocion

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -15,24 +15,23 @@ def main(event, context):
     image = vision.Image(source=vision.ImageSource(image_uri=blob_uri))
     response = client.face_detection(image=image)
     faceAnnotation = response.face_annotations
-    print("Entro Aqui" + str(faceAnnotation))
 
     #Get the first face
     analist = faceAnnotation[0]
+    answer = "no se sabe"
 
-    if (analist.detection_confidence >= 3) and (analist.detection_confidence <= 5):
-        answer = "confident"
-    elif (analist.anger_likelihood >= 3) and (analist.anger_likelihood <= 5):
-        answer = "angry"
-    elif (analist.joy_likelihood >= 3) and (analist.joy_likelihood <= 5):
-        answer = "happy"
-    elif (analist.sorrow_likelihood >= 3) and (analist.sorrow_likelihood <= 5):
-        answer = "sad"
-    elif (analist.surprise_likelihood >= 3) and (analist.surprise_likelihood <= 5):
-        answer = "surprised"
+    if (analist.detection_confidence >= 4) and (analist.detection_confidence <= 5):
+        answer = "Confiado"
+    elif (analist.anger_likelihood >= 4) and (analist.anger_likelihood <= 5):
+        answer = "Enojado"
+    elif (analist.joy_likelihood >= 4) and (analist.joy_likelihood <= 5):
+        answer = "Feliz"
+    elif (analist.sorrow_likelihood >= 4) and (analist.sorrow_likelihood <= 5):
+        answer = "Triste"
+    elif (analist.surprise_likelihood >= 4) and (analist.surprise_likelihood <= 5):
+        answer = "Sorprendido"
 
     employee = file_name.split(".")[0]
-    print(employee + " is " + answer)
 
     app_options = {"projectId": "sentimentproject-362601"}
     app = firebase_admin.initialize_app(options=app_options)
@@ -43,6 +42,7 @@ def main(event, context):
         "name": employee,
         "emotion": answer
     })
+    print(employee + " esta " + answer)
     
     # Delete the default app 
     firebase_admin.delete_app(app)
