4f8fd85deb288f83bd34b5ef00a16885fcd54b2d
Mariana <marianavargas1209@gmail.com>
2022-09-17 02:19:44 -0600

    [edit] pipeline modificado funcional y main documentado

--- cloud_function/main.py
+++ cloud_function/main.py
@@ -1,19 +1,25 @@
+#Importaciones necesarias
 from google.cloud import vision
 import firebase_admin
 from firebase_admin import firestore
+#Inicio de funcion
 def main(event, context):
+    #variables de nombre de la persona y bucket donde se almacena la imagen
     file_name = event["name"]
     bucket_name = event["bucket"]
+    #API del IA
     client = vision.ImageAnnotatorClient()
-    # URI of the image
+    # URI de la imagen
     blob_uri = f"gs://{bucket_name}/{file_name}"
-    # blob source
+    # blob source, como un objeto
     image = vision.Image(source=vision.ImageSource(image_uri=blob_uri))
     response = client.face_detection(image=image)
+    #respuesta del sentimiento
     face_annotation = response.face_annotations
-    print("Entro Aqui" + str(face_annotation))
-    # Get the first face
+    # Toma solo la primera cara
     analist = face_annotation[0]
+    #Compara el resultado para cada sentimiento
+    #El estado varia de 0 a 5
     answer = ["no se sabe"]
     if (analist.detection_confidence >= 4) and (analist.detection_confidence <= 5):
         answer += ["confiado"]
@@ -26,6 +32,7 @@ def main(event, context):
     if (analist.surprise_likelihood >= 4) and (analist.surprise_likelihood <= 5):
         answer += ["sorprendido"]
     employee = file_name.split(".")[0]
+    #Separa todos los estados de la personas
     resp = ''
     if len(answer) == 1:
         resp = answer[0]
@@ -35,7 +42,9 @@ def main(event, context):
                 resp += person + ", "
     employee = file_name.split(".")[0]
     print(employee + " esta " + str(resp))
+    #app donde se encuenta el bucket
     app_options = {"projectId": "sentimentproject-362601"}
+    #Almacena los resultados en la base de datos
     app = firebase_admin.initialize_app(options=app_options)
     database = firestore.client()
     doc = database.collection("employee").document(employee)
@@ -43,6 +52,6 @@ def main(event, context):
         "name": employee,
         "emotions": resp
     })
-    # Delete the default app
+    # Elimina el default app
     firebase_admin.delete_app(app)
     return employee + " esta " + str(resp)
